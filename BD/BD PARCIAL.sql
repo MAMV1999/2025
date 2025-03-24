9e388173ec717827bf1533ddbd7ff7aaf900267f
MAMV1999 <mmanriquevarillas25@gmail.com>
2025-03-24 00:24:28 -0500

    240325_1

--- BD/BD PARCIAL.sql
+++ BD/BD PARCIAL.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 21-03-2025 a las 22:13:58
+-- Tiempo de generación: 24-03-2025 a las 06:23:42
 -- Versión del servidor: 10.1.31-MariaDB
 -- Versión de PHP: 7.2.3
 
