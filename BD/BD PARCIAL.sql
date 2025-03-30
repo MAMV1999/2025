df7ee23fcbf57b78635484c5416663a3163d2da2
MAMV1999 <mmanriquevarillas25@gmail.com>
2025-03-30 00:22:28 -0500

    300325_1

--- BD/BD PARCIAL.sql
+++ BD/BD PARCIAL.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-03-2025 a las 04:35:59
+-- Tiempo de generación: 30-03-2025 a las 07:21:26
 -- Versión del servidor: 10.1.31-MariaDB
 -- Versión de PHP: 7.2.3
 
@@ -979,13 +979,13 @@ ALTER TABLE `almacen_producto`
 -- AUTO_INCREMENT de la tabla `almacen_salida`
 --
 ALTER TABLE `almacen_salida`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=176;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=182;
 
 --
 -- AUTO_INCREMENT de la tabla `almacen_salida_detalle`
 --
 ALTER TABLE `almacen_salida_detalle`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=357;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=366;
 
 --
 -- AUTO_INCREMENT de la tabla `documento`
@@ -1117,7 +1117,7 @@ ALTER TABLE `usuario_cargo`
 -- AUTO_INCREMENT de la tabla `usuario_docente`
 --
 ALTER TABLE `usuario_docente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `usuario_documento`
--- Registro/Vista/Escritorio.php
+++ Registro/Vista/Escritorio.php
@@ -13,15 +13,83 @@ if (!isset($_SESSION['nombre'])) {
 
         <!-- CUERPO_INICIO -->
         <div class="my-3 p-3 bg-body rounded shadow-sm" id="listado">
-            <h5 class="border-bottom pb-2 mb-0"><b>REGISTRO</b></h5>
+            <h5 class="border-bottom pb-2 mb-0"><b>UTILES ESCOLARES</b></h5>
             <div class="d-flex text-body-secondary pt-3">
                 <br>
                 <?php
                 $array = array(
                     "1" => array("nombre" => "REGISTRO UTILES ESCOLARES", "link" => "reg_documento.php"),
-                    "2" => array("nombre" => "REGISTRO ASISTENCIAS", "link" => "reg_documento.php"),
-                    "3" => array("nombre" => "REGISTRO JUSTIFICACIONES", "link" => "reg_documento.php"),
-                    "4" => array("nombre" => "REGISTRO INCIDENCIAS", "link" => "reg_documento.php"),
+                );
+                ?>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">NOMBRE</th>
+                            <th scope="col">PAGUINA</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $a = 1;
+                        while ($a <= count($array)) {
+                            echo '<tr>
+                                    <th scope="row">' . $a . '</th>
+                                    <td>' . $array[$a]["nombre"] . '</td>
+                                    <td><a class="btn btn-primary" href="' . $array[$a]["link"] . '" role="button">Ir a ' . $array[$a]["nombre"] . '</a></td>
+                                </tr>';
+                            $a++;
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <!-- CUERPO_INICIO -->
+        <div class="my-3 p-3 bg-body rounded shadow-sm" id="listado">
+            <h5 class="border-bottom pb-2 mb-0"><b>ASISTENCIAS / JUSTIFICACIONES</b></h5>
+            <div class="d-flex text-body-secondary pt-3">
+                <br>
+                <?php
+                $array = array(
+                    "1" => array("nombre" => "REGISTRO ASISTENCIAS", "link" => "reg_documento.php"),
+                    "2" => array("nombre" => "REGISTRO JUSTIFICACIONES", "link" => "reg_documento.php"),
+                );
+                ?>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">NOMBRE</th>
+                            <th scope="col">PAGUINA</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php
+                        $a = 1;
+                        while ($a <= count($array)) {
+                            echo '<tr>
+                                    <th scope="row">' . $a . '</th>
+                                    <td>' . $array[$a]["nombre"] . '</td>
+                                    <td><a class="btn btn-primary" href="' . $array[$a]["link"] . '" role="button">Ir a ' . $array[$a]["nombre"] . '</a></td>
+                                </tr>';
+                            $a++;
+                        }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+
+        <!-- CUERPO_INICIO -->
+        <div class="my-3 p-3 bg-body rounded shadow-sm" id="listado">
+            <h5 class="border-bottom pb-2 mb-0"><b>INCIDENCIAS</b></h5>
+            <div class="d-flex text-body-secondary pt-3">
+                <br>
+                <?php
+                $array = array(
+                    "1" => array("nombre" => "REGISTRO INCIDENCIAS", "link" => "reg_documento.php"),
                 );
                 ?>
                 <table class="table">
